dce989fd789fc6cb905f1874242dd5b9d8f07f89
dashmoment <dashmoment1017@gmail.com>
2015-01-04 10:34:02 +0800

    none

--- pla.py
+++ pla.py
@@ -50,7 +50,16 @@ class find_pla:
 
 
 
-                
+pla_dat = np.loadtxt('pla_data.csv',delimiter=',')
+
+x,y = np.array(pla_dat[:,:2]),pla_dat[:,2].astype(np.int)
+
+new_pla = find_pla([-x[0][1]/x[0][0],1],x,y)
+
+for i in range(10):
+    new_pla.update()
+    new_pla.plot()
+         
                 
                
         
